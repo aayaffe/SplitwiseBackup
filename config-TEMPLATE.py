616a2163e2c64e4ad47b3a295734770e96530486
Amit Yaffe <aayaffe@gmail.com>
2021-09-06 20:48:50 +0300

    Added a configuration template file

---
+++ config-TEMPLATE.py
@@ -0,0 +1,10 @@
+consumer_key = ""  # Instructions: https://dev.splitwise.com/#section/Authentication
+consumer_secret = ""
+API_key = ""
+group_id = -1
+language = 'he'  # 'en'
+download_images_pdf = True
+xlsx_filename = f'export/expenses.xlsx'
+json_filename = f'export/expenses.json'
+receipts_dir = f'export/receipts/'
+expenses_list_limit = 50000
